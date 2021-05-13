d2844db4cdb8b8f4b447dcac3450576e6cab64d6
illfate <illfate254@gmail.com>
2021-05-14 01:12:17 +0300

    Add buffer

--- internal/body/blinker.go
+++ internal/body/blinker.go
@@ -27,8 +27,8 @@ func NewService(blinkedTime int) *Processor {
 	return &Processor{
 		blinkedTime:   blinkedTime,
 		blinkTimeout:  5,
-		noseCrookedCh: make(chan struct{}),
-		backCrookedCh: make(chan struct{}),
+		noseCrookedCh: make(chan struct{}, 100),
+		backCrookedCh: make(chan struct{}, 100),
 	}
 }
 
